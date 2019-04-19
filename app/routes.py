edff9ccca1efdf99d99ff0f1319bc6e6b28483fe
Thomas <thomas_crawford96@hotmail.com>
2019-04-19 15:47:08 +0800

    added basic html pages we will need

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,18 @@ from app import app
 def index():
     user = {'username': 'user'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/account')
+def account():
+    """Renders the account page."""
+    return render_template('account.html')
+
+@app.route('/polls')
+def polls():
+    """Renders the polls page."""
+    return render_template('polls.html')
+
+@app.route('/register')
+def register():
+    """Renders the register page."""
+    return render_template('register.html')
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,9 @@
 // custom scripts
+$(document).ready(function () {
+    $("#last_modified").text(document.lastModified + " - Last Modified"); //don't think this actually does the right thing
+    $("#current_time").text(new Date().toLocaleString() + " - Current Time");
+});
+
+function redirect(url) {
+    window.location.href = url;
+}
