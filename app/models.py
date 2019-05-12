a53b0972cf077730024ae452e5c8186d0fedebc1
Archy Nayoan <archylefree.n@outlook.com>
2019-05-13 06:23:45 +0800

    fixed login required, changed admin required

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
-from app import db, login
+from app import db, login_manager
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 # query functions
-@login.user_loader
+@login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
--- app/routes.py
+++ app/routes.py
@@ -41,15 +41,17 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
-# logout, login required
-@login_required
+# logout
 @app.route('/logout')
 def logout():
-    logout_user()
+    if current_user.is_authenticated:
+        logout_user()
+    else:
+        flash('Already logged out!')
     return redirect(url_for('index'))
 
 
-# renders register
+# renders register for new account
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
@@ -67,100 +69,112 @@ def register():
 
 
 # TODO
-# renders account, login required
+# renders a user's account page, login required
 @app.route('/account')
+@login_required
 def account():
-    return render_template('account.html', title='My Account')
+    return render_template('account.html', title='My Account', user=current_user)
 
 
 # TODO
-# renders polls, login required
+# renders all polls for standard users, login required
 @app.route('/polls')
+@login_required
 def polls():
-    return render_template('polls.html', title='Polls')
+    return render_template('polls.html', title='Polls', polls=Poll)
 
 
-# TODO
 # admin page to manage polls
 @app.route('/manage_polls')
+@login_required
 def manage_polls():
-    return render_template(
-        'manage_polls.html',
-        title='Manage Polls',
-        poll=Poll,
-        recipe=Recipe)
+    if current_user.is_admin:
+        return render_template('manage_polls.html', title='Manage Polls', polls=Poll, recipes=Recipe)
+    else:
+        abort(403) # error 403 forbidden, no access if not admin
 
 
-# TODO
 # admin page to manage users
 @app.route('/manage_users')
+@login_required
 def manage_users():
-    return render_template('manage_users.html', title='Manage Users')
-
-
-# TODO
-# admin page to add recipes
-@app.route('/add_recipe', methods=['GET', 'POST'])
-def add_recipe():
-    form = RecipesCreationForm()
-
-    form.poll.choices = [
-        (poll.id, poll.name) for poll in Poll.query.order_by('date_created')]
-
-    if form.validate_on_submit():
-        recipe = Recipe(
-            name=form.name.data,
-            description=form.description.data,
-            contributor_id=current_user.id,
-            poll_id=form.poll.data
-            )
-
-        db.session.add(recipe)  # adds to the database
-        db.session.commit()  # commits all the changes in the database
-        flash('Added to database')
-        return redirect(url_for('add_recipe'))
-
-    return render_template('add_recipe.html', title='Add Recipe', form=form)
+    if current_user.is_admin:
+        return render_template('manage_users.html', title='Manage Users', users=User)
+    else:
+        abort(403)
 
 
 # TODO
 # admin page to add polls
 @app.route('/add_poll', methods=['GET', 'POST'])
+@login_required
 def add_poll():
-    form = PollCreationForm()
+    if current_user.is_admin:
+        form = PollCreationForm()
 
-    if form.validate_on_submit():
-        poll = Poll(
-            name=form.name.data,
-            description=form.description.data,
-            creator_id=current_user.id
-        )
-
-        db.session.add(poll)
-        db.session.flush()  # lets you access the generated poll.id
-
-        for r in form.recipes:
-            recipe = Recipe(
-                name=r.data['name'],
-                description=r.data['description'],
-                contributor_id=current_user.id,
-                poll_id=poll.id
+        if form.validate_on_submit():
+            poll = Poll(
+                name=form.name.data,
+                description=form.description.data,
+                creator_id=current_user.id
             )
 
-            db.session.add(recipe)
+            db.session.add(poll)
+            db.session.flush()  # lets you access the generated poll.id
 
-        db.session.commit()  # commits all the changes in the database
-        flash('Added to database')
-        return redirect(url_for('add_poll'))
+            for r in form.recipes:
+                recipe = Recipe(
+                    name=r.data['name'],
+                    description=r.data['description'],
+                    contributor_id=current_user.id,
+                    poll_id=poll.id
+                )
 
-    return render_template('add_poll.html', title='Add Poll', form=form)
+                db.session.add(recipe)
+
+            db.session.commit()  # commits all the changes in the database
+            flash('Added to database')
+            return redirect(url_for('add_poll'))
+
+        return render_template('add_poll.html', title='Add Poll', form=form)
+    else:
+        abort(403)
+
+
+# TODO
+# admin page to add recipes
+@app.route('/add_recipe', methods=['GET', 'POST'])
+@login_required
+def add_recipe():
+    if current_user.is_admin:
+        form = RecipesCreationForm()
+
+        form.poll.choices = [
+            (poll.id, poll.name) for poll in Poll.query.order_by('date_created')]
+
+        if form.validate_on_submit():
+            recipe = Recipe(
+                name=form.name.data,
+                description=form.description.data,
+                contributor_id=current_user.id,
+                poll_id=form.poll.data
+                )
+
+            db.session.add(recipe)  # adds to the database
+            db.session.commit()  # commits all the changes in the database
+            flash('Added to database')
+            return redirect(url_for('add_recipe'))
+
+        return render_template('add_recipe.html', title='Add Recipe', form=form)
+    else:
+        abort(403)
 
 
 # renders the page for a poll with given id
-@login_required
 @app.route('/poll/<int:poll_id>')
+@login_required
 def poll(poll_id):
-    if User.query.filter_by(username=current_user.username).first().is_admin:
+    if current_user.is_admin:
         poll = Poll.query.get_or_404(poll_id)
         return render_template('poll.html', title=poll.name + ' - Poll', users=User, poll=poll)
     else:
@@ -169,10 +183,10 @@ def poll(poll_id):
 
 # TODO
 # updates a poll with given id
-@login_required
 @app.route("/poll/<int:poll_id>/update", methods=['GET', 'POST'])
+@login_required
 def update_poll(poll_id):
-    if User.query.filter_by(username=current_user.username).first().is_admin:
+    if current_user.is_admin:
         return -1
     else:
         abort(403)
@@ -180,20 +194,20 @@ def update_poll(poll_id):
 
 # TODO
 # delete a poll with given id
-@login_required
 @app.route("/poll/<int:post_id>/delete", methods=['POST'])
+@login_required
 def delete_post(poll_id):
-    if User.query.filter_by(username=current_user.username).first().is_admin:
+    if current_user.is_admin:
         return -1
     else:
         abort(403)
 
 
 # renders the page for a recipe with given id
-@login_required
 @app.route('/recipe/<int:recipe_id>')
+@login_required
 def recipe(recipe_id):
-    if User.query.filter_by(username=current_user.username).first().is_admin:
+    if current_user.is_admin:
         recipe = Recipe.query.get_or_404(recipe_id)
         return render_template('recipe.html', title=recipe.name + ' - Recipe', users=User, polls=Poll, recipe=recipe)
     else:
@@ -202,10 +216,10 @@ def recipe(recipe_id):
 
 # TODO
 # updates a recipe with given id
-@login_required
 @app.route("/recipe/<int:recipe_id>/update", methods=['GET', 'POST'])
+@login_required
 def update_recipe(recipe_id):
-    if User.query.filter_by(username=current_user.username).first().is_admin:
+    if current_user.is_admin:
         return -1
     else:
         abort(403)
@@ -213,10 +227,10 @@ def update_recipe(recipe_id):
 
 # TODO
 # delete a recipe with given id
-@login_required
 @app.route("/recipe/<int:recipe_id>/delete", methods=['POST'])
+@login_required
 def delete_recipe(recipe_id):
-    if User.query.filter_by(username=current_user.username).first().is_admin:
+    if current_user.is_admin:
         return -1
     else:
         abort(403)
